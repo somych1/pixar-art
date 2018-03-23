6601a5bd3d02b34e33efb01d33b61663b4daa2a6
Samat Baltin <somych1@gmail.com>
2018-03-23 15:19:13 -0500

    changed input color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
+const button = $('#set-color')
 
+const colorField = $('#color-field')
+console.log()
+
+button.on('click', function(event){
+	console.log(event)
+	event.preventDefault()
+	const color = colorField.val()
+	$('.brush').css('background-color', color)
+	// console.log(e.target)
+	// console.log(e.currentTarget)
+	// const theButton = $(e.currentTarget)
+})
+
+
+// $('#color-field').on("input", function(event){
+// 	console.log('it work')
+// });
